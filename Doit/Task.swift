c47820fe21ed63731b97914bb321a6e8dc52079c
Joel Reynolds <reynolds.joel@gmail.co>
2017-09-08 10:28:23 +1000

    lecture 59. do it list with one exclamation point

---
+++ Doit/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  Doit
+//
+//  Created by Joel Reynolds on 7/9/17.
+//  Copyright © 2017 Joel Reynolds. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
--- Doit/ViewController.swift
+++ Doit/ViewController.swift
@@ -12,10 +12,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBOutlet weak var tableView: UITableView!
     
+    var tasks : [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        tasks = makeTasks()
         
         tableView.dataSource = self
         tableView.delegate = self
@@ -23,15 +26,40 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        //print(tasks.count)
+        return tasks.count
+        
     
     }
+    
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Hello"
+        let task = tasks[indexPath.row]
+        if task.important {
+            cell.textLabel?.text = "❗️\(task.name)"
+        }
+        else {
+            cell.textLabel?.text = task.name
+        }
         return cell
         
+    }
+    
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        task1.name = "Walk the dog"
+        task1.important = false
+        
+        let task2 = Task()
+        task2.name = "Buy cheese"
+        task2.important = true
+        
+        let task3 = Task()
+        task3.name = "Mow the lawn"
+        task3.important = false
+        
+        return [task1, task2, task3]
     }
 
 
